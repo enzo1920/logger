97aa28025c7a6b465bfb6bab8249ab8febcfd94b
enzo1920 <enzo1920@ya.ru>
2018-10-20 23:07:58 +0300

    abs_counter

--- log_analyzer.py
+++ log_analyzer.py
@@ -3,6 +3,7 @@
 import re
 import gzip
 import os
+import datetime
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
@@ -14,30 +15,86 @@ config = {
     "LOG_DIR": "./log"
 }
 
+
+
+# на вход подавать имя файла
 def reader():
     #проверяем наличие файла в директории
     #print(os.path.dirname(os.path.abspath(__file__)))
+    
+    list_dict=[]#список со словарями
+    rx = re.compile(r'(?:GET|POST)\s+(/\S+)')
     file_log=os.path.dirname(os.path.abspath(__file__))+'/logs/nginx-access-ui.log-20170630.gz'
     print(file_log)
     if os.path.isfile(file_log):
      try:
        with gzip.open(file_log,'r') as gfile:
+           lines_cnt = 0 #ограничемся пока 1000 строк
            for line in gfile:
-                ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', line )
-                print('>>>>', line)
-                print(ip)
+                ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', line[:16] )
+                urls = rx.findall(line)
+                time=float(line[-6:])
+                #print(str(time)+' '+urls[0])
+                time_urls={}# словарь содержит url и время
+                try:
+                   #ога тут оказывается, может быть пусто, занчит меняли формат
+                  if len(urls)!=0:
+                    time_urls["url"]=urls[0]
+                    time_urls["time"]=time
+                  else:
+                    time_urls["url"]='/xxxxx'
+                    time_urls["time"]=0
+                except Exception as exc:
+                       print(urls)
+                       print(str(len(urls)))
+                list_dict.append(time_urls)
+                lines_cnt+=1
+                if(lines_cnt==100000):
+                   break
+
+                  
      except KeyboardInterrupt:
                print("прерывание с клавиатуры")
-     #finally:
-            #file_log.close()
+
     else:
          print('file not found')
+         list_dict=0
+    return list_dict
 
+# simple file counter
+def str_counter():
+    file_log=os.path.dirname(os.path.abspath(__file__))+'/logs/nginx-access-ui.log-20170630.gz'
+    if os.path.isfile(file_log):
+       with gzip.open(file_log,'r') as gfile:
+           lines_cnt = 0 #ограничемся пока 1000 строк
+           for line in gfile:
+               lines_cnt+=1
+           print(lines_cnt)
 
+#Функция подсчета статистики по url'ам. На вход подается лист словарей list[{"url":url},{"time":time}]
+def url_abscounter():
+    list_urls=reader()#получаем список словарей со значениями
+    dict_absstat={} 
+    try:
+      for row in list_urls:
+         if row["url"] in dict_absstat.keys():
+              dict_absstat[row["url"]]+=1
+         else:
+            dict_absstat[row["url"]]=1
+    except Exception as exc:
+                 print('-----error-->>'+str(line_err))
+    #пробуем посчитать максимум и миниму из словарями
+    key_max = max(dict_absstat.keys(), key=(lambda k: dict_absstat[k]))
+    key_min = min(dict_absstat.keys(), key=(lambda k: dict_absstat[k]))
+
+    print('Maximum Value: ',dict_absstat[key_max])
+    print('Minimum Value: ',dict_absstat[key_min])
+     #print(dict_absstat)
 
 def main():
     #pass
-    reader()
+    url_abscounter()
+    #str_counter()
 
 if __name__ == "__main__":
     main()
