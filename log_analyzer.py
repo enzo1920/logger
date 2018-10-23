9200d7965a243ddb86287a3c1339bc8a8edab41e
enzo1920 <enzo1920@ya.ru>
2018-10-23 12:42:33 +0300

    work with return errors.

--- log_analyzer.py
+++ log_analyzer.py
@@ -4,6 +4,7 @@ import re
 import gzip
 import os
 import datetime
+import sys
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
@@ -12,7 +13,8 @@ import datetime
 config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
-    "LOG_DIR": "./log"
+    "LOG_DIR": "./log",
+    "max_err": 115,
 }
 
 
@@ -42,7 +44,8 @@ def reader():
                     time_urls["url"]=urls[0]
                     time_urls["time"]=time
                   else:
-                    print('error parsing url in str'+str(lines_cnt))
+                    print('error parsing url in str '+str(lines_cnt))
+                    error_counter+=1
                     #time_urls["url"]='/xxxxx'
                     #time_urls["time"]=0
                 except Exception as exc:
@@ -50,7 +53,7 @@ def reader():
                        print(str(len(urls)))
                 list_dict.append(time_urls)
                 lines_cnt+=1
-                #if(lines_cnt==1000000):
+                #if(lines_cnt==602095):
                    #print(lines_cnt)
                    #break
 
@@ -61,7 +64,7 @@ def reader():
     else:
          print('file not found')
          list_dict=0
-    return list_dict
+    return list_dict,error_counter
 
 # simple file counter
 #def str_counter():
@@ -88,32 +91,47 @@ def time_decorator(original_func):
 
 #Функция подсчета статистики по url'ам. На вход подается лист словарей list[{"url":url},{"time":time}]
 @time_decorator
-def url_abscounter():
-    list_urls=reader()#получаем список словарей со значениями
-    dict_absstat={} 
+def url_abscounter(list_urls, err_cnt):
     lines_cnt= 0 #ограничемся пока 1000 строк
-    for row in list_urls:
-        try:
-         if row["url"] in dict_absstat:#.keys():
-              dict_absstat[row["url"]]+=1
-         else:
-            dict_absstat[row["url"]]=1
-         lines_cnt+=1
-        except Exception as exc:
-                 print('-----error count-->>')
-    #пробуем посчитать максимум и миниму из словарями
-    key_max = max(dict_absstat.keys(), key=(lambda k: dict_absstat[k]))
-    key_min = min(dict_absstat.keys(), key=(lambda k: dict_absstat[k]))
-    print(lines_cnt)
-    print('Maximum Value: ',dict_absstat[key_max])
-    print('Minimum Value: ',dict_absstat[key_min])
-     #print(dict_absstat)
+    max_val=0 # максимальное значение урлов
+    min_val=0 # минимальное значение урлов
+    dict_absstat={}#словарь, в который будем записывать суммарное количество вхождений урлов
+    print('config max error is: '+str(config['max_err'])+' analysis error is: '+str(err_cnt))
+    if(err_cnt<=config['max_err']):
+          for row in list_urls:
+              try:
+                 if row["url"] in dict_absstat:#.keys():
+                     dict_absstat[row["url"]]+=1
+                 else:
+                     dict_absstat[row["url"]]=1
+                 lines_cnt+=1
+              except Exception as exc:
+                       print('-----error count in -->> '+str(lines_cnt))
+          #пробуем посчитать максимум и минимум из словарями
+          key_max = max(dict_absstat.keys(), key=(lambda k: dict_absstat[k]))
+          key_min = min(dict_absstat.keys(), key=(lambda k: dict_absstat[k]))
+          max_val=dict_absstat[key_max]
+          min_val=dict_absstat[key_min]
+          print('Cnt Value: ',str(lines_cnt))
+          print('Maximum Value: ',str(max_val))
+          print('Minimum Value: ',str(min_val))
+    else:   
+          print('Analysis: your file contins more errors than set in config')
+          print(lines_cnt)
 
+
+    #return  max_val, min_val, lines_cnt
 
 
 def main():
     #pass
-    url_abscounter()
+    #max,min,cnt=
+    lsturls, errcnt=reader()#получаем список словарей со значениями и количество ошибок при парсинге
+    url_abscounter(lsturls,errcnt)
+    #print('Cnt Value: ',cnt)
+    #print('Maximum Value: ',str(max))
+    #print('Minimum Value: ',str(min))
+
     #str_counter()
 
 if __name__ == "__main__":
