91d15a1bf902cbb8ec34e5619056edd6546ab4ee
enzo1920 <enzo1920@ya.ru>
2018-10-28 23:41:51 +0300

    using new parse for url and time

--- log_analyzer.py
+++ log_analyzer.py
@@ -16,18 +16,30 @@ config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log",
-    "max_err": 115,
+    "max_err": 700,
 }
 
 
+#декоратор время выполнения
+def benchmark(original_func):
+    def wrapper(*args, **kwargs):
+        start = datetime.datetime.now()
+        result = original_func(*args, **kwargs)
+        end = datetime.datetime.now()
+        print('{0} is executed in {1}'.format(original_func.__name__, end-start))
+        return result
+    return wrapper
+
 
 # на вход подавать имя файла
+@benchmark
 def reader():
     #проверяем наличие файла в директории
     #print(os.path.dirname(os.path.abspath(__file__)))
     error_counter=0#счетчик ошибок
     list_dict=[]#список со словарями
-    rx = re.compile(r'(?:GET|POST)\s+(/\S+)')
+    #rx = re.compile(r'(?:GET|POST)\s+(/\S+)')
+    rx = re.compile(r'/\/(.*?)\/\?/ism')
     file_log=os.path.dirname(os.path.abspath(__file__))+'/logs/nginx-access-ui.log-20170630.gz'
     print(file_log)
     if os.path.isfile(file_log):
@@ -35,8 +47,10 @@ def reader():
        with gzip.open(file_log,'r') as gfile:
            lines_cnt = 0 #ограничемся пока 1000 строк
            for line in gfile:
-                ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', line[:16] )
-                urls = rx.findall(line)
+                #ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', line[:16] )
+                finded =line.split(' ')
+                print(finded[7])
+                urls =finded[7] #rx.findall(finded[7])
                 time=float(line[-6:])
                 #print(str(time)+' '+urls[0])
                 time_urls={}# словарь содержит url и время
@@ -44,22 +58,26 @@ def reader():
                 stat_dict={}
                 try:
                    #ога тут оказывается, может быть пусто, занчит меняли формат
-                  if len(urls)!=0:
-                    stat_dict["url_time"]=time#добавляем в словарь статистики время
-                    stat_dict["cnt"]=0#добавляем в словарь статистики количество 0
-                    time_urls["url"]=urls[0]
-                    time_urls["stat"]=stat_dict #ключ- урл, значение- список со статистикой 
+                  if len(urls[0])!=0:
+                        stat_dict["url_time"]=time#добавляем в словарь статистики время
+                        stat_dict["cnt"]=0#добавляем в словарь статистики количество 0
+                        #time_urls["url"]=urls[0]
+                        time_urls["url"]=urls
+                        time_urls["stat"]=stat_dict #ключ- урл, значение- список со статистикой 
                   else:
-                    print('error parsing url in str '+str(lines_cnt))
+                    #print('error parsing url in str '+str(lines_cnt))
+                    print('error parsing url in str '+str(urls[0]))
                     error_counter+=1
                     #time_urls["url"]='/xxxxx'
                     #time_urls["time"]=0
                 except Exception as exc:
                        #print(urls)
-                       print(str(len(urls)))
+                       print(exc)
                 list_dict.append(time_urls)
                 lines_cnt+=1
-                if(lines_cnt==2500):
+                print(lines_cnt)
+                if(lines_cnt==1000000):
+                   #print(time)
                    print(lines_cnt)
                    break
 
@@ -84,16 +102,6 @@ def reader():
 #           print(lines_cnt)
 
 
-#декоратор время выполнения
-def benchmark(original_func):
-    def wrapper(*args, **kwargs):
-        start = datetime.datetime.now()
-        result = original_func(*args, **kwargs)
-        end = datetime.datetime.now()
-        print('{0} is executed in {1}'.format(original_func.__name__, end-start))
-        return result
-    return wrapper
-
 
 #Функция подсчета статистики по url'ам. На вход подается лист словарей list[{"url":url},{"time":time}]
 @benchmark
@@ -108,28 +116,30 @@ def url_abscounter(list_urls, err_cnt):
               try:
                  #print(dict_row["stat"] )
                  if dict_row["url"] in dict_absstat:#.keys():
-                                  print("find!!!!")
+                                  print("find!!!!"+dict_row["url"])
                                   #time.sleep(5)
                                   dict_absstat.update({dict_row["url"]:
-                                                       {"time":dict_absstat["time"]+dict_row["stat"]["url_time"],
-                                                        "cnt":dict_absstat["cnt"]+1,
-                                                        "min":min([dict_absstat[dict_row["url"]]],dict_absstat["min"]),
-                                                        "max":max([dict_absstat[dict_row["url"]]],dict_absstat["max"])}})
+                                                       {"time":dict_absstat[dict_row["url"]]["time"]+dict_row["stat"]["url_time"],
+                                                        "cnt":dict_absstat[dict_row["url"]]["cnt"]+1 }})#,
+#                                                        "min":min([dict_absstat[dict_row["url"]]],dict_absstat[dict_row["url"]]["min"]),
+#                                                        "max":max([dict_absstat[dict_row["url"]]],dict_absstat[dict_row["url"]]["max"])}})
 
                  else:
                                   #print("2")
                       dict_absstat.update({dict_row["url"]:
                                                            {"time":dict_row["stat"]["url_time"],
-                                                           "cnt":1,
-                                                           "min":dict_row["stat"]["url_time"],
-                                                           "max":dict_row["stat"]["url_time"]}})
+                                                           "cnt":dict_row["stat"]["cnt"]+1}})#,
+#                                                           "min":dict_row["stat"]["url_time"],
+#                                                           "max":dict_row["stat"]["url_time"]}})
                       #dict_row["stat"]["cnt"]=1
                       #print(dict_row["stat"]["cnt"])
 
                       #dict_absstat[row["url"]]=1
                  
               except Exception as exc:
-                       print('-----error count in -->> '+str(exc))
+                       print(exc)
+                       print(dict_row)
+                       #print('-----error2  is -->> '+str(dict_row["stat"]["url_time"]))
           #пробуем посчитать максимум и минимум из словарями
           #key_max = max(dict_absstat.keys(), key=(lambda k: dict_absstat[k]))
           #key_min = min(dict_absstat.keys(), key=(lambda k: dict_absstat[k]))
@@ -151,22 +161,22 @@ def avg_counter(abs_dict):
         #Пробуем посчитать средние значения
         urls_cnt=0
         for k,v in abs_dict.iteritems():
-             #print(k)
+             print('unique url: '+str(urls_cnt))
              urls_cnt+=1
         #всего урл:
         print('totat distinct url: '+str(urls_cnt))
-        print(abs_dict)
+        #print(abs_dict)
 
         for k,v in abs_dict.iteritems():
                abs_dict[k]["percent_count"]=100*float(abs_dict[k]["cnt"])/float(urls_cnt)
-        print(abs_dict)
+        #print(abs_dict)
 
 def main():
     #pass
-    #max,min,cnt=
     lsturls, errcnt=reader()#получаем список словарей со значениями и количество ошибок при парсинге
     abs_dict=url_abscounter(lsturls,errcnt)
     avg_counter(abs_dict)
+    print("Error count is: "+str(errcnt))
 
     #sys.exit()
     #print('Cnt Value: ',cnt)
