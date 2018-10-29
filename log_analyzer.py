30bdd6351bbdaa31aa6e80fd43f51294158c8780
enzo1920 <enzo1920@ya.ru>
2018-10-29 23:44:44 +0300

    all parametrs is counted!

--- log_analyzer.py
+++ log_analyzer.py
@@ -16,7 +16,7 @@ config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log",
-    "max_err": 700,
+    "max_err": 30,
 }
 
 
@@ -31,6 +31,26 @@ def benchmark(original_func):
     return wrapper
 
 
+#функция сравнения двух  максимальногозначения времени
+#rel_tol - относительная толерантность, умножается на большую величину двух аргументов;
+#по мере того как значения становятся больше, то и допустимая разность между ними при этом считается равной.
+#abs_tol - абсолютный допуск, который применяется как-во всех случаях. 
+#Если разница меньше любой из этих допусков, значения считаются равными. 
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+    if(abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)):
+     return b
+    else:
+     return a
+
+#функция поиска медианы
+def median(lst):
+    n = len(lst)
+    if n < 1:
+            return None
+    if n % 2 == 1:
+            return sorted(lst)[n//2]
+    else:
+            return sum(sorted(lst)[n//2-1:n//2+1])/2.0
 # на вход подавать имя файла
 @benchmark
 def reader():
@@ -38,6 +58,7 @@ def reader():
     #print(os.path.dirname(os.path.abspath(__file__)))
     error_counter=0#счетчик ошибок
     unique_urlcnt=0#счетчик уникальных урлов
+    timetotal_url=0#счетчик уникальных урлов
     time_urls={}# словарь содержит url и время
     #rx = re.compile(r'(?:GET|POST)\s+(/\S+)')
     rx = re.compile(r'/\/(.*?)\/\?/ism')
@@ -50,50 +71,46 @@ def reader():
            for line in gfile:
                 #ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', line[:16] )
                 finded =line.split(' ')
-                #print(finded[7])
+                #считаем согласно формату, что урл 7, если он не 7 значит формат менялся, очевидно нужно добавить регулярку , чтобы заматчить
                 urls =finded[7].strip() #rx.findall(finded[7])
                 time=float(line[-6:])
                 #print(str(time)+' '+urls[0])
 
                 #словарь статистики состоит из:time_url-время урла , url_count количества
-                stat_dict={}
+                time_list=[] #список времени для урла,по нему будем считать медиану
                 try:
-                   #ога тут оказывается, может быть пусто, занчит меняли формат
+                  #проверяем ли не пуст урл
                   if len(urls)!=0: 
                      if urls in time_urls:
+                         time_list=time_urls[urls]['time_mass']
+                         time_list.append(time)
                          time_urls.update({urls:
-                                                 {"time":time_urls[urls]['time']+time,
-                                                   "cnt":time_urls[urls]["cnt"]+1}})
+                                                 {"time_sum":time_urls[urls]['time_sum']+time,
+                                                  "time_mass":time_list,
+                                                  "cnt":time_urls[urls]["cnt"]+1}})
                          unique_urlcnt+=1
-                         #print(urls)
-                        #stat_dict["url_time"]=time#добавляем в словарь статистики время
-                        #stat_dict["cnt"]=0#добавляем в словарь статистики количество 0
-                        #time_urls["url"]=urls[0]
-                        #time_urls["url"]=urls
-                        #time_urls["stat"]=stat_dict #ключ- урл, значение- список со статистикой 
+
                      else:
+                         time_list.append(time)
                          time_urls.update({urls:
-                                                 {"time":time,
-                                                   "cnt":0}})
+                                                 {"time_sum":time,
+                                                  "time_mass":time_list,
+                                                  "cnt":1}})
                          #print(time_urls)
 
                   else:
                        print('url len=0')
                        error_counter+=1
-                    #print('error parsing url in str '+str(lines_cnt))
-                    #print('error parsing url in str '+str(urls[0]))
-                  #error_counter+=1
-                  #print(time_urls)
-                    #time_urls["url"]='/xxxxx'
-                    #time_urls["time"]=0
+
                 except Exception as exc:
                        #print(urls)
                        error_counter+=1
-                       print(exc)
+                       print(str(exc))
                 lines_cnt+=1
-                #if(lines_cnt==1000000):
-                   #print(lines_cnt)
-                   #break
+                timetotal_url+=time
+                if(lines_cnt==2000):
+                   print(lines_cnt)
+                   break
 
                   
      except KeyboardInterrupt:
@@ -101,54 +118,51 @@ def reader():
 
     else:
          print('file not found')
-         list_dict=0
-    #print(list_dict)
-    return time_urls,error_counter,unique_urlcnt,lines_cnt
-
-# simple file counter
-#def str_counter():
-#    file_log=os.path.dirname(os.path.abspath(__file__))+'/logs/nginx-access-ui.log-20170630.gz'
-#    if os.path.isfile(file_log):
-#       with gzip.open(file_log,'r') as gfile:
-#           lines_cnt = 0 #ограничемся пока 1000 строк
-#           for line in gfile:
-#               lines_cnt+=1
-#           print(lines_cnt)
+         time_urls={}
+         error_counter=config["max_err"]
+         unique_urlcnt=0,
+         lines_cnt=0
+         timetotal_url=0
+    return time_urls,error_counter,unique_urlcnt,lines_cnt,timetotal_url
 
 
-
-#функция для подсчета процентов на вход словарь с абсолютными занчения abs_counter
+#функция для подсчета процентов на вход словарь с урлами и временем, на выходе словарь урл, время, процент процентами 
 @benchmark
-def avg_counter(dict):
-        #Пробуем посчитать средние значения
-        urls_cnt=0
+def percent_url_counter(dict,uniq_url,time_sum,err):
+#считаем статистику для урла
+    if(err<=config["max_err"]):
         for k,v in dict.iteritems():
-             #print('unique url: '+str(urls_cnt))
-             urls_cnt+=1
-        #всего урл:
-        print('totat distinct url: '+str(urls_cnt))
-        #print(abs_dict)
+               dict[k]["count_perc"]=100*float(dict[k]["cnt"])/float(uniq_url)
+               dict[k]["time_perc"]=100*dict[k]["time_sum"]/float(time_sum)
+               dict[k]["time_avg"]=dict[k]["time_sum"]/dict[k]["cnt"]
+               #dict[k]["time_med"]=median(dict[k]["time list"])
+               list_to_max=dict[k]["time_mass"]
+               #максимум времени
+               dict[k]["time_max"]=max(list_to_max)
+               #медиана времени
+               dict[k]["time_med"]=median(list_to_max)
+    else:
 
-        #for k,v in abs_dict.iteritems():
-               #abs_dict[k]["percent_count"]=100*float(abs_dict[k]["cnt"])/float(urls_cnt)
-        #print(abs_dict)
+        dict={}
+        print("Много ошибок")
+        #попробуем посчитать топ по 5 самым частым урлам
+        #n = max(dict.values())
+        #print range(n-10,n+1)[::-1]
+    return dict
 
 def main():
-    pass
-    #reader()
-    timeurls, errcnt,uniquecnt,total_cnt=reader()#получаем список словарей со значениями и количество ошибок при парсинге
-    #abs_dict=url_abscounter(lsturls,errcnt)
-    #print(timeurls)
+    #pass
+    timeurls, errcnt,uniquecnt,total_cnt,total_time=reader()#получаем  словарЬ со значениями и количество ошибок при парсинге
+    final_dict=percent_url_counter(timeurls,uniquecnt,total_time,errcnt)
     print("Error count is: "+str(errcnt))
-    print("uniquecnt  is: "+str(uniquecnt))
+    print("unique count  is: "+str(uniquecnt))
     print("total count str  is: "+str(total_cnt))
+    print("total time url   is: "+str(total_time))
+    #list=[0.3,0.155,0,0.112,0.8,0.1,0.2,4.7]
+    for k,v in final_dict.iteritems():
+        if(final_dict[k]["cnt"]>=2):
+            print(k,v)
 
-    #sys.exit()
-    #print('Cnt Value: ',cnt)
-    #print('Maximum Value: ',str(max))
-    #print('Minimum Value: ',str(min))
-
-    #str_counter()
 
 if __name__ == "__main__":
     main()
